58906277cea63351299a2862ff1b978246a3166e
RawzaDigital <rawzadigital@gmail.com>
2019-08-09 21:36:11 +0700

    add click listener button to MainActivity and test it with toast

--- app/src/main/java/net/kampungweb/alarmmanager/MainActivity.java
+++ app/src/main/java/net/kampungweb/alarmmanager/MainActivity.java
@@ -1,14 +1,71 @@
 package net.kampungweb.alarmmanager;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
+    Button btnOneTimeAlarmDate;
+    TextView tvOneTimeAlarmDate;
+    Button btnOneTimeAlarmTime;
+    TextView tvOneTimeAlarmTime;
+    EditText edtOneTimealarmMessage;
+    Button btnSetOneTimeAlarm;
+
+    Button btnRepeatingAlarmTime;
+    TextView tvRepeatingTimeAlarmTime;
+    EditText edtRepeatingAlarmMessage;
+    Button btnSetRepeatingAlarm;
+    Button btnCancelAlarm;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnOneTimeAlarmDate = findViewById(R.id.btn_one_time_alarm_date);
+        tvOneTimeAlarmDate = findViewById(R.id.tv_one_time_alarm_date);
+        btnOneTimeAlarmTime = findViewById(R.id.btn_one_time_alarm_time);
+        tvOneTimeAlarmTime = findViewById(R.id.tv_one_time_alarm_time);
+        edtOneTimealarmMessage = findViewById(R.id.edt_one_time_alarm_message);
+        btnSetOneTimeAlarm = findViewById(R.id.btn_set_one_time_alarm);
+
+        btnRepeatingAlarmTime = findViewById(R.id.btn_repeating_time_alarm_time);
+        tvRepeatingTimeAlarmTime = findViewById(R.id.tv_repeating_time_alarm_time);
+        edtRepeatingAlarmMessage = findViewById(R.id.edt_repeating_alarm_message);
+        btnSetRepeatingAlarm = findViewById(R.id.btn_set_repeating_alarm);
+        btnCancelAlarm = findViewById(R.id.btn_cancel_alarm);
+
+        btnOneTimeAlarmDate.setOnClickListener(this);
+        btnOneTimeAlarmTime.setOnClickListener(this);
+        btnSetOneTimeAlarm.setOnClickListener(this);
+        btnRepeatingAlarmTime.setOnClickListener(this);
+        btnSetRepeatingAlarm.setOnClickListener(this);
+        btnCancelAlarm.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btn_one_time_alarm_date) {
+            Toast.makeText(getApplicationContext(), "Clicked", Toast.LENGTH_SHORT).show();
+        } else if (view.getId() == R.id.btn_one_time_alarm_time) {
+            Toast.makeText(getApplicationContext(), "Clicked", Toast.LENGTH_SHORT).show();
+        } else if (view.getId() == R.id.btn_set_one_time_alarm) {
+            Toast.makeText(getApplicationContext(), "clicked", Toast.LENGTH_SHORT).show();
+        } else if (view.getId() == R.id.btn_repeating_time_alarm_time) {
+            Toast.makeText(getApplicationContext(), "clicked", Toast.LENGTH_SHORT).show();
+        } else if (view.getId() == R.id.btn_set_repeating_alarm) {
+            Toast.makeText(getApplicationContext(), "clicked", Toast.LENGTH_SHORT).show();
+        } else if (view.getId() == R.id.btn_cancel_alarm) {
+            Toast.makeText(getApplicationContext(), "clicked", Toast.LENGTH_SHORT).show();
+        }
     }
 }
