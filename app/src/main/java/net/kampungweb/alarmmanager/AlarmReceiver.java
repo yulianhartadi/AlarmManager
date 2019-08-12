08a27a80d710e2f6777260e4972dff4f05d62427
RawzaDigital <rawzadigital@gmail.com>
2019-08-12 15:00:17 +0700

    showAlarmNotification() new method, try adding new with no deprecated notification

--- app/src/main/java/net/kampungweb/alarmmanager/AlarmReceiver.java
+++ app/src/main/java/net/kampungweb/alarmmanager/AlarmReceiver.java
@@ -1,8 +1,21 @@
 package net.kampungweb.alarmmanager;
 
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
+
+import java.util.Calendar;
 
 /**
  * TODO 3:
@@ -11,13 +24,92 @@ import android.content.Intent;
  */
 public class AlarmReceiver extends BroadcastReceiver {
 
-    public static  final String TYPE_ONE_TIME = "OneTimeAlarm";
+    public static final String TYPE_ONE_TIME = "OneTimeAlarm";
+    public static final String TYPE_REPEATING = "RepeatingAlarm";
+    public static final String EXTRA_MESSAGE = "message";
+    public static final String EXTRA_TYPE = "type";
+    private final int NOTIF_ID_ONETIME = 100;
+    private final int NOTIF_ID_REPEATING = 101;
 
+    public AlarmReceiver() {
+
+    }
 
     @Override
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+        String type = intent.getStringExtra(EXTRA_TYPE);
+        String message = intent.getStringExtra(EXTRA_MESSAGE);
+        String title = type.equalsIgnoreCase(TYPE_ONE_TIME) ? "One Time Alarm" : "Repeating Alarm";
+        int notifId = type.equalsIgnoreCase(TYPE_ONE_TIME) ? NOTIF_ID_ONETIME : NOTIF_ID_REPEATING;
+        showAlarmNotification(context, title, message, notifId);
     }
+
+    private void showAlarmNotification(Context context, String title, String message, int notifId) {
+        NotificationManager notificationManagerCompat = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        // Deprecated
+        /*
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.ic_access_alarm)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setColor(ContextCompat.getColor(context, android.R.color.transparent))
+                .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000})
+                .setSound(alarmSound);
+        notificationManagerCompat.notify(notifId, builder.build());
+        */
+
+        // try adding new with no deprecated notification from stackoverflow
+        // https://stackoverflow.com/questions/45462666/notificationcompat-builder-deprecated-in-android-o
+        NotificationCompat.Builder builder = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel notificationChannel = new NotificationChannel("ID", "name", importance);
+            notificationManagerCompat.createNotificationChannel(notificationChannel);
+            builder = new NotificationCompat.Builder(context, notificationChannel.getId());
+        } else {
+            builder = new NotificationCompat.Builder(context);
+        }
+
+        builder = builder
+                .setSmallIcon(R.drawable.ic_access_alarm)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setColor(ContextCompat.getColor(context, android.R.color.transparent))
+                .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000})
+                .setSound(alarmSound);
+        notificationManagerCompat.notify(notifId, builder.build());
+
+    }
+
+    public void setOneTimeAlarm(Context context, String type, String date, String time, String message){
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, AlarmReceiver.class);
+        intent.putExtra(EXTRA_TYPE, type);
+        String dateArray[] = date.split("-");
+        String timeArray[] = time.split(":");
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, Integer.parseInt(dateArray[0]));
+        calendar.set(Calendar.MONTH, Integer.parseInt(dateArray[1]), -1);
+        calendar.set(Calendar.DAY_OF_MONTH, Integer.parseInt(dateArray[2]));
+        calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(timeArray[0]));
+        calendar.set(Calendar.MINUTE, Integer.parseInt(timeArray[1]));
+        calendar.set(Calendar.SECOND,0);
+        int requestCode = NOTIF_ID_ONETIME;
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, requestCode, intent, 0);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+        Toast.makeText(context, "One time alarm setup", Toast.LENGTH_SHORT).show();
+    }
+
+    public void cancelAlarm(Context context, String type){
+        AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, AlarmReceiver.class);
+        int requestCode = type.equalsIgnoreCase(TYPE_ONE_TIME) ? NOTIF_ID_ONETIME : NOTIF_ID_REPEATING;
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, requestCode, intent, 0);
+        alarmManager.cancel(pendingIntent);
+    }
+
 }
